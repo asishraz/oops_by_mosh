5fc575f877603c8cafcdfee58e29f312bbd3582f
asishraz <iamasish3001@gmail.com>
2021-05-26 16:35:30 +0530

    section1 ends

--- section1/index.js
+++ section1/index.js
@@ -1,1 +1,22 @@
-console.log("hello");
+// encapsulation example
+
+let baseSal = 3000;
+let overTime = 20;
+let rate = 30;
+
+function getWage(baseSal,overTime,rate) {
+    return baseSal + (overTime * rate);
+};
+
+// can be changed to 
+
+let employee = {
+    baseSal: 3000,
+    overTime: 20,
+    rate: 30,
+    getWage: function() {
+        return this.baseSal + (this.overTime * this.rate);
+    }
+};
+
+console.log(employee.getWage());
