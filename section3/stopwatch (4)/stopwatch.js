1e25f6bcb60d7eee37f417c4f2dc79c19cf6edfb
asishraz <iamasish3001@gmail.com>
2021-06-01 20:40:33 +0530

    section4 started

--- section3/stopwatch (4)/stopwatch.js
+++ section3/stopwatch (4)/stopwatch.js
@@ -1,35 +1,38 @@
-function Stopwatch() { 
-  let startTime, endTime, running, duration = 0;
+function Stopwatch() {
+  let startTime,
+    endTime,
+    running,
+    duration = 0;
 
-  this.start = function() {
-    if (running) 
-      throw new Error('Stopwatch has already started.');
-    
-    running = true; 
+  this.start = function () {
+    if (running) throw new Error("Stopwatch has already started.");
+
+    running = true;
 
     startTime = new Date();
   };
 
-  this.stop = function() {
-    if (!running) 
-      throw new Error('Stopwatch is not started.');
+  this.stop = function () {
+    if (!running) throw new Error("Stopwatch is not started.");
 
-    running = false; 
-      
+    running = false;
+
     endTime = new Date();
 
     const seconds = (endTime.getTime() - startTime.getTime()) / 1000;
-    duration += seconds; 
+    duration += seconds;
   };
 
-  this.reset = function() { 
+  this.reset = function () {
     startTime = null;
     endTime = null;
-    running = false; 
-    duration = 0; 
+    running = false;
+    duration = 0;
   };
 
-  Object.defineProperty(this, 'duration', {
-    get: function() { return duration; }
+  Object.defineProperty(this, "duration", {
+    get: function () {
+      return duration;
+    },
   });
 }
